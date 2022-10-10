e23a5ac5b235f251dc139e8df74ebef87215120d
ShanmukhSaiMadhu <madhushanmukhsai@gmail.com>
2022-10-10 20:58:39 +0530

    pages created

---
+++ src/components/HomePageComponents/LandingPageComponent/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+
+function LandingPageComponent() {
+  return <div className="wrapper">HI hello world</div>;
+}
+
+export default LandingPageComponent;
