ba48ff5d5d7148dd86088ebdcfc17337e2461153
ShanmukhSaiMadhu <madhushanmukhsai@gmail.com>
2022-10-13 23:15:56 +0530

    chart js

--- src/components/DashboardComponents/LineChart/index.js
+++ src/components/DashboardComponents/LineChart/index.js
@@ -1,8 +1,9 @@
 import React from "react";
-import "./styles.js";
+import "./styles.css";
 import { Line } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
 
-function LineChart({ data, options }) {
+function LineChart({ chartData, options }) {
   return (
     <div>
       <Line data={chartData} options={options} />;
