ccc02f4102686b46d3bd9be4c78b462855f425be
ShanmukhSaiMadhu <madhushanmukhsai@gmail.com>
2022-10-19 23:49:44 +0530

    toggle added

--- src/pages/CoinPage.js
+++ src/pages/CoinPage.js
@@ -12,16 +12,17 @@ import { getDaysArray } from "../functions/getDaysArray";
 import { getPrices } from "../functions/getPrices";
 import { getPriorDate } from "../functions/getPriorDate";
 import { getCoinData } from "../functions/getCoinData";
+import ColorToggleButton from "../components/CoinPageComponents/Toggle";
 
 function CoinPage() {
   const [searchParams] = useSearchParams();
-
   const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
   const [loadingChart, setLoadingChart] = useState(true);
   const [coin, setCoin] = useState({});
   const [days, setDays] = useState(30);
   const [prices, setPrices] = useState([]);
+  const [type, setType] = useState("prices");
   const today = new Date();
   const priorDate = new Date(new Date().setDate(today.getDate() - days));
 
@@ -61,14 +62,10 @@ function CoinPage() {
 
   const getData = async () => {
     const response_data = await getCoinData(searchParams, true);
-
     setData(response_data);
-
     const prices_data = await getPrices(response_data.id, days);
-
     setPrices(prices_data)
     var dates = getDaysArray(priorDate, today);
-
     prices_data?.[`${days}`][1] < prices_data?.[0][1] 
     ? setChartData({
       labels: dates,
@@ -119,7 +116,6 @@ function CoinPage() {
     setDays(event.target.value);
     const prices_data = await getPrices(data.id, event.target.value);
     setPrices(prices_data);
-
     const priorDate = getPriorDate(event.target.value);
     var dates = getDaysArray(priorDate, today);
 
@@ -153,15 +149,29 @@ function CoinPage() {
           <Header />
           <CoinPageList coin={coin} delay={2} />
           <div className="coin-page-div">
-            <div className="graph-btns">
-              <ButtonGroup variant="outlined" size="medium" aria-label="medium button group">
-                <Button onClick={handleChange} value={7}>1W</Button>
-                <Button onClick={handleChange} value={30}>1M</Button>
-                <Button onClick={handleChange} value={60}>3M</Button>
-                <Button onClick={handleChange} value={365}>1Y</Button>
-              </ButtonGroup>
+            <div className="toggle-wrapper">
+              <div className="toggle-flex">
+                <ColorToggleButton
+                  type={type}
+                  setType={setType}
+                  days={days}
+                  chartData={chartData}
+                  setChartData={setChartData}
+                  id={data.id}
+                />
+              </div>
+              <div className="graph-btns">
+                <ButtonGroup variant="outlined" size="medium" aria-label="medium button group">
+                  <Button onClick={handleChange} value={7}>1W</Button>
+                  <Button onClick={handleChange} value={30}>1M</Button>
+                  <Button onClick={handleChange} value={60}>3M</Button>
+                  <Button onClick={handleChange} value={365}>1Y</Button>
+                </ButtonGroup>
             </div>
+            </div>
+            
             <LineChart chartData={chartData} options={options} />
+            
           </div>
           
           <CoinPageDesc name={data.name} desc={data.description.en} />
