6a6a8d710a3a17df03311951ee6adf903f2d0ebd
ShanmukhSaiMadhu <madhushanmukhsai@gmail.com>
2022-10-20 23:21:47 +0530

    footer 1

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,23 @@
 import React from "react";
 import styles from "./styles.css";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import EmailIcon from "@mui/icons-material/Email";
+import InsertLinkIcon from "@mui/icons-material/InsertLink";
+import TwitterIcon from "@mui/icons-material/Twitter";
 function Footer() {
-  return <div id="footer" className="footer-wrapper"></div>;
+  return (
+    <div id="footer" className="footer-wrapper">
+      <h1 className="heading">CryptoTracker.</h1>
+      <div className="socials">
+        <InstagramIcon style={{ fontSize: "2rem" }} />
+        <FacebookIcon style={{ fontSize: "2rem" }} />
+        <TwitterIcon style={{ fontSize: "2rem" }} />
+        <EmailIcon style={{ fontSize: "2rem" }} />
+        <InsertLinkIcon style={{ fontSize: "2rem" }} />
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
